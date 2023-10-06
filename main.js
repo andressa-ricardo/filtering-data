53dffa792940cf7676e4d4e79214495b452de725
andressa-ricardo <andressaricardo30@gmail.com>
2023-10-06 16:39:28 -0300

    feat: i took the input selector and searched mug

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   await page.goto("https://www.amazon.com.br/ref=nav_logo");
+
+  await page.waitForSelector("#twotabsearchtextbox");
+  await page.type("#twotabsearchtextbox", "Caneca", { delay: 100 });
+  await page.click("#nav-search-submit-button");
+  await page.waitForNavigation();
 
   await browser.close();
 })();
